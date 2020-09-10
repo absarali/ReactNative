128337936e6942d8a5c423a6efb436e399253d86
absarali <absarali62123@gmail.com>
2020-09-10 16:46:15 +0500

    Task 05

--- Round 02/Week 01/Task 05(Video 59)/screens/Add.js
+++ Round 02/Week 01/Task 05(Video 59)/screens/Add.js
@@ -1,42 +1,46 @@
-import React, { useState} from 'react'
-import { View, StyleSheet, Text} from 'react-native'
-
+import React, {useReducer} from 'react'
+import { View, Text, StyleSheet} from 'react-native'
+const reducer = (state, action) =>{
+    switch(action.type){
+        case 'increment': return{...state, count: state.count +1} 
+        case 'decrement': return{...state, count: state.count -1}
+        default: return state
+    };
+};
 const Add = () => {
-    const [state, dispatch] = useState(0)
+    const [state, dispatch] = useReducer(reducer, {count: 0})
     return(
-            <View>
-                <Text style = {styles.head}>Use Reducer</Text>
-                <Text style = {styles.button} onPress={
-                    () => {
-                        dispatch(state + 1)
-                    }
-                }>Increase Counter</Text>
-                <Text style = {styles.button} onPress={
-                    () => {
-                        dispatch(state - 1)
-                    }
-                }>Decrease Counter</Text>
-    <Text style = {styles.text}>Counter Vaue: {state}</Text>
-            </View>
-    )
+        <View>
+            <Text style={styles.head}>Use Reducer</Text>
+            <Text style={styles.button} onPress={
+                () => {
+                    dispatch({type:'increment'})
+                }
+            }>Increase</Text>
+            <Text style={styles.button} onPress={
+                () => {
+                    dispatch({type:'decrement'})
+                }
+            }>Decrease</Text>
+    <Text style={styles.text}>Current Value: {state.count}</Text>
+        </View>
+    );
 };
 const styles = StyleSheet.create({
-    head: {
-        marginBottom: 50,
+    head:{
+        marginBottom: 20,
+        fontSize: 35,
         textAlign: 'center',
-        fontSize: 30
     },
-    button: {
-        borderRadius: 20,
-        borderColor: 'blue',
+    button:{
         padding: 10,
-        fontSize: 18,
-        color: 'rgb(50,50,255)',
+        fontSize: 20,
+        color: 'darkturquoise',
         textAlign: 'center'
     },
-    text: {
-        textAlign: 'center',
-        fontSize: 16
+    text:{
+        fontSize: 16,
+        textAlign: 'center'
     }
 });
-export default Add
+export default Add;
